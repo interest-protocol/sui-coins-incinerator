d459842856a2139bb8e009a0a9b410f0be657192
git-marcopitra <marcomorete73@hotmail.com>
2024-12-20 09:51:06 +0100

    :mag: feat: improve SEO

--- components/seo/index.tsx
+++ components/seo/index.tsx
@@ -1,47 +1,110 @@
 import Head from 'next/head';
 import { FC } from 'react';
 
-import { SEOProps } from './seo.types';
-
-const SEO: FC<SEOProps> = ({ pageTitle }) => {
-  const title = `${pageTitle} | Sui Coins`;
+const SEO: FC = () => {
+  const title = `Incinerator | Sui Coins`;
 
   return (
     <Head>
-      <meta name="theme-color" content="#282828" />
       <meta charSet="utf-8" />
       <meta name="title" content={title} />
+      <meta name="theme-color" content="#fff" />
       <meta
         name="description"
-        content="Generate custom coins and list all your tokens."
+        content="Burn your assets and keep your wallet clean."
       />
-      <meta property="og:locale" content="en_US" />
       <meta property="og:title" content={title} />
-      <meta property="og:site_name" content="SUI Coins" />
+      <meta property="og:locale" content="en_US" />
+      <meta property="og:site_name" content="Sui Coins Incinerator" />
       <meta
         property="og:description"
-        content="Generate custom coins and list all your tokens."
+        content="Burn your assets and keep your wallet clean."
       />
-      <meta
-        property="og:image"
-        content="https://metaschool.so/assets/landing-page.webp"
-      />
-      <meta
-        property="og:image:secure_url"
-        content="/android-chrome-256x256.png"
-      />
+      <meta property="og:image" content="/banner.webp" />
+      <meta property="og:image:secure_url" content="/banner.webp" />
       <meta property="og:image:type" content="image/png" />
-      <meta property="og:image:width" content="256" />
-      <meta property="og:image:height" content="256" />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="675" />
       <meta property="og:image:alt" content={title} />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content="SUI Coins" />
-      <meta name="twitter:site" content="https://suicoins.com/" />
-      <meta name="twitter:image" content="/ms-icon-310x310.png" />
+      <meta name="twitter:site" content="https://incinerator.suicoins.com/" />
+      <meta name="twitter:image" content="/banner.webp" />
       <meta
         name="twitter:description"
         content="Generate custom coins and list all your tokens."
       />
+      <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png" />
+      <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png" />
+      <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png" />
+      <link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png" />
+      <link
+        rel="apple-touch-icon"
+        sizes="114x114"
+        href="/apple-icon-114x114.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="120x120"
+        href="/apple-icon-120x120.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="144x144"
+        href="/apple-icon-144x144.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="152x152"
+        href="/apple-icon-152x152.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-icon-180x180.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="192x192"
+        href="/android-icon-192x192.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="96x96"
+        href="/favicon-96x96.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        href="/favicon-96x96.png"
+        sizes="96x96"
+      />
+      <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+      <link rel="shortcut icon" href="/favicon.ico" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <meta name="apple-mobile-web-app-title" content="Incinerator" />
+      <link rel="manifest" href="/site.webmanifest" />
+      <meta name="msapplication-TileColor" content="#ffffff" />
+      <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
+      <meta name="theme-color" content="#ffffff"></meta>
       <title>{title}</title>
     </Head>
   );
