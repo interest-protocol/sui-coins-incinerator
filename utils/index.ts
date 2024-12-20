86eff6b074332372a43c26aaf09aa14c860211af
git-marcopitra <marcomorete73@hotmail.com>
2024-12-20 05:10:15 +0100

    :bug: fix: remove info

--- utils/index.ts
+++ utils/index.ts
@@ -3,7 +3,6 @@ export * from './coin';
 export * from './coin-metadata';
 export * from './fn';
 export * from './number';
-export * from './swr';
 export * from './tag';
 export * from './toast';
 export * from './tx';
